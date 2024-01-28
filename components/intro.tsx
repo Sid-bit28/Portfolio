0243da2fe2a0ddf1cdf75bf32176723290995090
Siddarth Banerjee <siddarth2801@gmail.com>
2024-01-29 01:11:41 +0530

    Intro Added

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,10 +3,14 @@
 import React from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithub, FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
     return (
-        <section>
+        <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div
@@ -39,6 +43,44 @@ export default function Intro() {
                     </motion.span>
                 </div>
             </div>
+            <motion.p
+                className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text:4xl"
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+            >
+                <span className="font-bold">Hello, I'm Siddarth.</span>
+                I'm a <span className="font-bold">full-stack developer</span>. I
+                enjoy making a building web related stuffs.
+                <span className="font-bold">
+                    Currently focused towards learning Next.js & TypeScript.
+                </span>
+            </motion.p>
+            <motion.div
+                className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                    delay: 0.1,
+                }}
+            >
+                <Link
+                    href="#contact"
+                    className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+                >
+                    {' '}
+                    Contact me here.
+                    <BsArrowRight className="opacity-70" />
+                </Link>
+                <a className="bg-white px-7 py-3 flex items-center gap-2 rounded-full">
+                    Download CV <HiDownload />
+                </a>
+                <a className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full">
+                    <BsLinkedin />
+                </a>
+                <a className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full">
+                    <FaGithubSquare />
+                </a>
+            </motion.div>
         </section>
     );
 }
